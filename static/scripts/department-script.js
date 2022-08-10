f9565f075fccd6ad46bdf0747f8cf386a807dbda
Daniel Footwear <danielfootwear@Daniels-MacBook-Pro.local>
2022-08-10 15:56:36 +0100

    update the collection page

--- static/scripts/department-script.js
+++ static/scripts/department-script.js
@@ -34,20 +34,16 @@ next.onclick = () => {
 
 // swipe to move image
 let touchstartX = 0;
-let touchstartY = 0;
 let touchendX = 0;
-let touchendY = 0;
 
 const gestureZone = document.getElementById('style-container');
 
 gestureZone.addEventListener('touchstart', function(event) {
     touchstartX = event.changedTouches[0].screenX;
-    touchstartY = event.changedTouches[0].screenY;
 }, false);
 
 gestureZone.addEventListener('touchend', function(event) {
     touchendX = event.changedTouches[0].screenX;
-    touchendY = event.changedTouches[0].screenY;
     handleGesture();
 }, false); 
 
